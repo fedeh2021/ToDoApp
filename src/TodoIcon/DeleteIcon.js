2361eb109c94ccace1e44821591c612e760891b5
Federico Hirsch Veloso <fhirsch@onfocusdev.com>
2023-03-15 18:31:36 -0300

    agregue alert + archivos

--- src/TodoIcon/DeleteIcon.js
+++ src/TodoIcon/DeleteIcon.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { TodoIcon } from "./";
+import Swal from "sweetalert2";
 
 function DeleteIcon({ onDelete }) {
-  return <TodoIcon type="delete" onClick={onDelete} />;
+  const handleDeleteClick = () => {
+    Swal.fire({
+      title: "¿Estás seguro que deseas eliminar esta tarea?",
+      showCancelButton: true,
+      confirmButtonText: "Eliminar",
+      cancelButtonText: "Cancelar",
+      icon: "warning",
+      dangerMode: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onDelete();
+      }
+    });
+  };
+
+  return <TodoIcon type="delete" onClick={handleDeleteClick} />;
 }
 
 export { DeleteIcon };
--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "../styles/TodosLoading.css";
-import loadingIcon from './icons8-reloj.gif'
+import loadingIcon from "./loading-icon.gif";
 
-const LOADING_CONTAINER_CLASS = 'LoadingTodo-container'
-const LOADING_TEXT_CLASS = 'LoadingTodo-text'
-const DELETE_ICON_CLASS = 'LoadingTodo-deleteIcon'
+const LOADING_CONTAINER_CLASS = "LoadingTodo-container";
+const LOADING_TEXT_CLASS = "LoadingTodo-text";
+const DELETE_ICON_CLASS = "LoadingTodo-deleteIcon";
+const LOADING_COMPLETE_ICON = "LoadingTodo-completeIcon";
 
 function TodosLoading() {
   return (
     <div className={LOADING_CONTAINER_CLASS}>
-      <img src={loadingIcon} alt='Loading icon' />
-      <span className="LoadingTodo-completeIcon"></span>
+      <img src={loadingIcon} alt="Loading icon" />
+      <span className={LOADING_COMPLETE_ICON}></span>
       <p className={LOADING_TEXT_CLASS}>Estamos cargando..</p>
       <span className={DELETE_ICON_CLASS}></span>
     </div>
